449e8f8a11d80741965e72ae48b6ceb4cc57b849
AlbertLin <chlin701005@gmail.com>
2022-05-16 09:16:34 +0800

    20220513

--- src/main/java/com/uuu/demo/demo2/Demo2Application.java
+++ src/main/java/com/uuu/demo/demo2/Demo2Application.java
@@ -2,10 +2,16 @@ package com.uuu.demo.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Demo2Application {
-
+	@RequestMapping("/")
+	public String home() {
+		return "hello spring boot from maven";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
